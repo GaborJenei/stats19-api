696ccce4514622ca338fd8c2629d07bd5ae81c1c
Gabor Jenei <jeneigabor14@gmail.com>
2021-04-29 07:54:52 +0100

    Database Model added

--- main.py
+++ main.py
@@ -1,6 +1,67 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Float
+import os
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+
+# Where the db is located in the system
+# This code will just return where the py file is in an os agnostic manner
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'planets.db')
+
+db = SQLAlchemy(app)
+
+
+@app.cli.command('db_create')
+def db_create():
+    db.create_all()
+    print('Database created!')
+
+
+@app.cli.command('db_drop')
+def db_drop():
+    db.drop_all()
+    print('Databased dropped!')
+
+
+@app.cli.command('db_seed')
+def db_seed():
+    mercury = Planet(planet_name='Mercury',
+                     planet_type='Class D',
+                     home_star='Sol',
+                     mass=3.258e23,
+                     radius=1516,
+                     distance=35.98e6)
+
+    venus = Planet(planet_name='Venus',
+                   planet_type='Class K',
+                   home_star='Sol',
+                   mass=4.867e24,
+                   radius=3760,
+                   distance=67.24e6)
+
+    earth = Planet(planet_name='Earth',
+                   planet_type='Class M',
+                   home_star='Sol',
+                   mass=5.972e24,
+                   radius=3959,
+                   distance=92.96e6)
+
+    db.session.add(mercury)
+    db.session.add(venus)
+    db.session.add(earth)
+
+    test_user = User(first_name='William',
+                     last_name='Herschel',
+                     email='test@test.com',
+                     password='P@assw0rd')
+
+    db.session.add(test_user)
+
+    db.session.commit()
+    print('Database seeded!')
 
 
 @app.route('/')
@@ -26,6 +87,43 @@ def parameters():
         return jsonify(message="Sorry " + name + " you are not old enough"), 401
     else:
         return jsonify(message="Welcome " + name + ", you are old enough!")
+
+
+@app.route('/url_variable/<string:name>/<int:age>')
+def url_variable(name: str, age: int):
+    if age < 18:
+        return jsonify(message="Sorry " + name + " you are not old enough"), 401
+    else:
+        return jsonify(message="Welcome " + name + ", you are old enough!")
+
+
+@app.route('/planets', methods=['GET'])
+def planets():
+    planets_list = Planet.query.all()
+    return jsonify(data=planets_list)
+
+
+# database models (could split it into a separate file)
+
+# This is a table in our model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+
+
+class Planet(db.Model):
+    __tablename__ = 'planets'
+    planet_id = Column(Integer, primary_key=True)
+    planet_name = Column(String)
+    planet_type = Column(String)
+    home_star = Column(String)
+    mass = Column(Float)
+    radius = Column(Float)
+    distance = Column(Float)
 
 
 if __name__ == '__main__':
